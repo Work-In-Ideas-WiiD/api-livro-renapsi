58f482672cc67bd9b8b48463ccfe41e064357eaf
ha1vy <macbook@MacBook-Pro-de-MACBOOK.local>
2024-08-09 08:41:40 -0300

    Correção da página de primeiro acesso

--- resources/js/pages/Register.vue
+++ resources/js/pages/Register.vue
@@ -1,33 +1,33 @@
 <template>
-<div class="main-content">
+<div class="main-content main-login">
     <!-- Header -->
-    <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
+    <div class="header py-7 py-lg-5 pt-lg-5">
       <div class="container">
-        <div class="header-body text-center mb-7">
+        <div class="header-body text-center">
           <div class="row justify-content-center">
             <div class="col-xl-5 col-lg-6 col-md-8 px-5">
-              <h1 class="text-white">Primeiro Acesso!</h1>
+              <h1 class="text-white tituloPrimeiro">Primeiro Acesso!</h1>
             </div>
           </div>
         </div>
       </div>
-      <div class="separator separator-bottom separator-skew zindex-100">
+      <!-- <div class="separator separator-bottom separator-skew zindex-100">
         <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
           <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
         </svg>
-      </div>
+      </div>  -->
     </div>
     <!-- Page content -->
     <div class="container mt--8 pb-5">
       <div class="row justify-content-center">
         <div class="col-lg-5 col-md-7">
-          <div class="card bg-secondary border-0 mb-0">
+          <div class="card bg-secondary border-0 mb-0 blocoLogin">
             <div class="card-header bg-transparent">
                <div class="text-center">
-                  <img src="assets/img/brand/Kaoa-preta.png" class="navbar-brand-img" alt="..." width="200px">
+                  <img src="assets/img/brand/assinatura-biblioteca-digital.png" class="navbar-brand-img" alt="..." width="200px">
                </div>
             </div>
-            <div class="card-body px-lg-5 py-lg-5">
+            <div class="card-body px-lg-5">
               <!-- <div class="text-center text-muted mb-4">
                 <small>Entre com suas credenciais</small>
               </div> -->
@@ -84,7 +84,7 @@
                   </span>
                 </div>
                 <div class="text-center">
-                  <button type="submit" class="btn btn-primary my-4">Cadastrar</button>
+                  <button type="submit" class="btn btn-primary my-4 botaoLogin">Cadastrar</button>
                 </div>
               </form>
             </div>
